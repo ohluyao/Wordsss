bf680948366c4564ab96e3333ba3a740cf0ac971
Kelvin Ren <renkelvin@gmail.com>
2011-10-05 18:59:20 +0800

    ~

---
+++ Wordsss/Word.m
@@ -0,0 +1,22 @@
+//
+//  Word.m
+//  Wordsss
+//
+//  Created by RenKelvin on 11-10-5.
+//  Copyright (c) 2011年 Ren Inc. All rights reserved.
+//
+
+#import "Word.h"
+
+
+@implementation Word
+@dynamic id;
+@dynamic word;
+@dynamic word_relateTo_wordRecord;
+@dynamic word_is_bookWord;
+@dynamic word_has_rootaffix;
+@dynamic word_is_listWord;
+@dynamic word_has_sense;
+@dynamic word_has_association;
+
+@end
